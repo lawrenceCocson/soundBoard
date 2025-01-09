492b1a12c10f153e0c09fe48dddb69c7e4bfa8e5
342478971 <342478971@C-MXL60108QB.tdsb.on.ca>
2025-01-09 15:14:24 -0500

    Buttons with letters

--- CocsonHaSoundboard/src/cocson/ha/GUIDriver.java
+++ CocsonHaSoundboard/src/cocson/ha/GUIDriver.java
@@ -11,40 +11,46 @@ import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.stage.Stage;
 
-
 public class GUIDriver extends Application {
 
 	@Override
 	public void start(Stage stage) throws Exception {
 		ArrayList<SoundPack> soundPacks = new ArrayList<>();
-		
+
 		GridPane grid = new GridPane();
 		grid.setHgap(10);
 		grid.setVgap(10);
-		grid.setAlignment(Pos.CENTER);
-		
+		grid.setAlignment(Pos.BOTTOM_LEFT);
+
+		String[] buttonNames = { "1", "2", "3", "4", "Q", "W", "E", "R", "A", "S", "D", "F", "Z", "X", "C", "V", };
+
 		for (int row = 0; row < 4; row++) {
 			for (int col = 0; col < 4; col++) {
-				Button button = new Button("Button " + (row * 4 + col + 1));
-				
+				Button button = new Button(buttonNames[row * 4 + col]);
+
+				button.setOnAction(e -> {
+
+					System.out.println(button.getText());
+
+					
+
+				});
 				grid.add(button, col, row);
 			}
+
 		}
-		
+
 		Scene scene = new Scene(grid, 400, 400);
-		
+
 		stage.setTitle("4x4 Grid of buttons");
 		stage.setScene(scene);
-		
-		
-		
+
 		stage.show();
 
 	}
 
 	public static void main(String[] args) {
 		launch(args);
-
 
 	}
 
