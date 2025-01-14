5e98762bd4b0b6a2e18fee0592e784f7539f67a8
342355419 <342355419@CL-MXL70111S4.tdsb.on.ca>
2025-01-14 13:53:37 -0500

    sound updates

--- CocsonHaSoundboard/src/cocson/ha/GUIDriver.java
+++ CocsonHaSoundboard/src/cocson/ha/GUIDriver.java
@@ -5,42 +5,58 @@ import java.util.ArrayList;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.media.Media;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.stage.Stage;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import java.util.Scanner;
+import java.io.File;
 
 public class GUIDriver extends Application {
 
 	@Override
 	public void start(Stage stage) throws Exception {
-		ArrayList<SoundPack> soundPacks = new ArrayList<>();
+		SoundPack verysickbeats = new SoundPack("verysickbeats");
+		
+		File soundPackFolder = new File("soundPacks");
+		File[] soundPacks = soundPackFolder.listFiles();
+		
+		System.out.println(soundPacks[0]);
 		
 		VBox vbox = new VBox(10);
 		
 		
 		Label soundboardname = new Label("SoundBoard");
 		Label sound = new Label("Sound");
+		HBox arrows = new HBox(2);
 		Button left = new Button("<-----");
 		Button right = new Button("----->");
+		
+		arrows.getChildren().addAll(left, right);
+		arrows.setAlignment(Pos.CENTER);
 		
 		
 
 		GridPane grid = new GridPane();
 		grid.setHgap(10);
 		grid.setVgap(10);
+		grid.setAlignment(Pos.CENTER);
 		vbox.setAlignment(Pos.CENTER);
 		
 		// Button names for each number
 
 		String[] buttonNames = { "1", "2", "3", "4", "Q", "W", "E", "R", "A", "S", "D", "F", "Z", "X", "C", "V"};
+		Button[] buttons = new Button[16];
+		Pad[] pads = new Pad[16];
 
 		for (int row = 0; row < 4; row++) {
 			for (int col = 0; col < 4; col++) {
 				Button button = new Button(buttonNames[row * 4 + col]);
 				button.setStyle("-fx-font-size: 2em; ");
+				buttons[row * 4 + col] = button;
 				
 
 				button.setOnAction(e -> {
@@ -58,7 +74,7 @@ public class GUIDriver extends Application {
 		
 		
 		
-		vbox.getChildren().addAll(soundboardname, sound, grid, left, right);
+		vbox.getChildren().addAll(soundboardname, sound, grid, arrows);
 		
 
 		Scene scene = new Scene(vbox,  400, 400);
