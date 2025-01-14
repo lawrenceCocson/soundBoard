7fd94d598e8e7b705770356c0a2f21cf1a3f19ad
342355419 <342355419@CL-MXL70111S4.tdsb.on.ca>
2025-01-14 13:29:12 -0500

    updates

--- CocsonHaSoundboard/src/cocson/ha/GUIDriver.java
+++ CocsonHaSoundboard/src/cocson/ha/GUIDriver.java
@@ -6,7 +6,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.stage.Stage;
@@ -19,23 +18,21 @@ public class GUIDriver extends Application {
 	public void start(Stage stage) throws Exception {
 		ArrayList<SoundPack> soundPacks = new ArrayList<>();
 		
-		SoundPack verysickbeats = new SoundPack("verysickbeats");
-		
-		verysickbeats.showSounds();
-		
 		VBox vbox = new VBox(10);
+		
 		
 		Label soundboardname = new Label("SoundBoard");
 		Label sound = new Label("Sound");
-		sound.setAlignment(Pos.CENTER);
-		soundboardname.setAlignment(Pos.CENTER);
+		Button left = new Button("<-----");
+		Button right = new Button("----->");
 		
 		
 
 		GridPane grid = new GridPane();
 		grid.setHgap(10);
 		grid.setVgap(10);
-		vbox.setAlignment(Pos.BOTTOM_LEFT);
+		vbox.setAlignment(Pos.CENTER);
+		
 		// Button names for each number
 
 		String[] buttonNames = { "1", "2", "3", "4", "Q", "W", "E", "R", "A", "S", "D", "F", "Z", "X", "C", "V"};
@@ -43,6 +40,8 @@ public class GUIDriver extends Application {
 		for (int row = 0; row < 4; row++) {
 			for (int col = 0; col < 4; col++) {
 				Button button = new Button(buttonNames[row * 4 + col]);
+				button.setStyle("-fx-font-size: 2em; ");
+				
 
 				button.setOnAction(e -> {
 
@@ -57,9 +56,24 @@ public class GUIDriver extends Application {
 		}
 		
 		
-		vbox.getChildren().addAll(soundboardname, sound, grid);
+		
+		
+		vbox.getChildren().addAll(soundboardname, sound, grid, left, right);
+		
 
 		Scene scene = new Scene(vbox,  400, 400);
+		
+		scene.setOnKeyPressed(event -> {
+            String pressedKey = event.getText().toUpperCase();  
+            for (int row = 0; row < 4; row++) {
+                for (int col = 0; col < 4; col++) {
+                    Button button = (Button) grid.getChildren().get(row * 4 + col);
+                    if (button.getText().equals(pressedKey)) {
+                        button.fire();  
+                    }
+                }
+            }
+        });
 
 		
 		stage.setScene(scene);
