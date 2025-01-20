017f4e12fa27ab2648ad1978914ef2165aafa923
342355419 <342355419@CL-MXL70111S4.tdsb.on.ca>
2025-01-20 13:53:52 -0500

    SoundPackIndex

---
+++ CocsonHaSoundboard/src/cocson/ha/SoundPackIndex.java
@@ -0,0 +1,26 @@
+package cocson.ha;
+
+import java.io.File;
+
+public class SoundPackIndex {
+	private int index;
+	private File[] inSoundFolder;
+	
+	SoundPackIndex(File[] inFile){
+		index = 0;
+		inSoundFolder = inFile;
+	}
+	
+	public int getIndex() {
+		return index;
+	}
+	
+	public void left(){
+		index -= 1;
+		if (index < 0) {
+			index = (int) inSoundFolder.length;
+		}
+		
+	}
+
+}
